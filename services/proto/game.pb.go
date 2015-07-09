14e1a9a4c44e04fecda8b8fc92b87df21d55fad8
xtaci <daniel820313@gmail.com>
2015-07-09 08:27:43 +0000

    update

--- services/proto/game.pb.go
+++ services/proto/game.pb.go
@@ -25,15 +25,7 @@ func (m *Game) Reset()         { *m = Game{} }
 func (m *Game) String() string { return proto1.CompactTextString(m) }
 func (*Game) ProtoMessage()    {}
 
-type Game_Nil struct {
-}
-
-func (m *Game_Nil) Reset()         { *m = Game_Nil{} }
-func (m *Game_Nil) String() string { return proto1.CompactTextString(m) }
-func (*Game_Nil) ProtoMessage()    {}
-
 type Game_Packet struct {
-	Uid     int32  `protobuf:"varint,1,opt" json:"Uid,omitempty"`
 	Content []byte `protobuf:"bytes,2,opt,proto3" json:"Content,omitempty"`
 }
 
@@ -48,7 +40,6 @@ func init() {
 
 type GameServiceClient interface {
 	Packet(ctx context.Context, opts ...grpc.CallOption) (GameService_PacketClient, error)
-	Notify(ctx context.Context, in *Game_Nil, opts ...grpc.CallOption) (GameService_NotifyClient, error)
 }
 
 type gameServiceClient struct {
@@ -90,43 +81,10 @@ func (x *gameServicePacketClient) Recv() (*Game_Packet, error) {
 	return m, nil
 }
 
-func (c *gameServiceClient) Notify(ctx context.Context, in *Game_Nil, opts ...grpc.CallOption) (GameService_NotifyClient, error) {
-	stream, err := grpc.NewClientStream(ctx, &_GameService_serviceDesc.Streams[1], c.cc, "/proto.GameService/Notify", opts...)
-	if err != nil {
-		return nil, err
-	}
-	x := &gameServiceNotifyClient{stream}
-	if err := x.ClientStream.SendMsg(in); err != nil {
-		return nil, err
-	}
-	if err := x.ClientStream.CloseSend(); err != nil {
-		return nil, err
-	}
-	return x, nil
-}
-
-type GameService_NotifyClient interface {
-	Recv() (*Game_Packet, error)
-	grpc.ClientStream
-}
-
-type gameServiceNotifyClient struct {
-	grpc.ClientStream
-}
-
-func (x *gameServiceNotifyClient) Recv() (*Game_Packet, error) {
-	m := new(Game_Packet)
-	if err := x.ClientStream.RecvMsg(m); err != nil {
-		return nil, err
-	}
-	return m, nil
-}
-
 // Server API for GameService service
 
 type GameServiceServer interface {
 	Packet(GameService_PacketServer) error
-	Notify(*Game_Nil, GameService_NotifyServer) error
 }
 
 func RegisterGameServiceServer(s *grpc.Server, srv GameServiceServer) {
@@ -159,27 +117,6 @@ func (x *gameServicePacketServer) Recv() (*Game_Packet, error) {
 	return m, nil
 }
 
-func _GameService_Notify_Handler(srv interface{}, stream grpc.ServerStream) error {
-	m := new(Game_Nil)
-	if err := stream.RecvMsg(m); err != nil {
-		return err
-	}
-	return srv.(GameServiceServer).Notify(m, &gameServiceNotifyServer{stream})
-}
-
-type GameService_NotifyServer interface {
-	Send(*Game_Packet) error
-	grpc.ServerStream
-}
-
-type gameServiceNotifyServer struct {
-	grpc.ServerStream
-}
-
-func (x *gameServiceNotifyServer) Send(m *Game_Packet) error {
-	return x.ServerStream.SendMsg(m)
-}
-
 var _GameService_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "proto.GameService",
 	HandlerType: (*GameServiceServer)(nil),
@@ -190,11 +127,6 @@ var _GameService_serviceDesc = grpc.ServiceDesc{
 			Handler:       _GameService_Packet_Handler,
 			ServerStreams: true,
 			ClientStreams: true,
-		},
-		{
-			StreamName:    "Notify",
-			Handler:       _GameService_Notify_Handler,
-			ServerStreams: true,
 		},
 	},
 }
