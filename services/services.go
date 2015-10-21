3a0c2bdd7cd40292f6bc83e7a29512cfe0dde76c
xtaci <daniel820313@gmail.com>
2015-10-21 14:57:52 +0800

    use offical etcd client

--- services/services.go
+++ services/services.go
@@ -8,8 +8,9 @@ import (
 	"sync/atomic"
 	"time"
 
-	"github.com/coreos/go-etcd/etcd"
+	etcdclient "github.com/coreos/etcd/client"
 	log "github.com/gonet2/libs/nsq-logger"
+	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 )
 
@@ -21,21 +22,24 @@ const (
 	RETRY_DELAY          = 10 * time.Second
 )
 
+// a single connection
 type client struct {
 	key  string
 	conn *grpc.ClientConn
 }
 
+// a kind of service
 type service struct {
 	clients []client
-	idx     uint32
+	idx     uint32 // for round-robin purpose
 }
 
+// all services
 type service_pool struct {
 	services          map[string]*service
-	service_names     map[string]bool // store names.txt
+	known_names       map[string]bool // store names.txt
 	enable_name_check bool
-	client_pool       sync.Pool
+	client            etcdclient.Client
 	sync.RWMutex
 }
 
@@ -55,29 +59,33 @@ func (p *service_pool) init() {
 	if env := os.Getenv("ETCD_HOST"); env != "" {
 		machines = strings.Split(env, ";")
 	}
-	p.client_pool.New = func() interface{} {
-		return etcd.NewClient(machines)
+
+	println(machines)
+	// init etcd client
+	cfg := etcdclient.Config{
+		Endpoints: machines,
+		Transport: etcdclient.DefaultTransport,
 	}
+	c, err := etcdclient.New(cfg)
+	if err != nil {
+		log.Critical(err)
+		os.Exit(-1)
+	}
+	p.client = c
 
+	// init
 	p.services = make(map[string]*service)
-
-	// load available names
+	p.known_names = make(map[string]bool)
 	p.load_names()
-	// connect all services
 	p.connect_all(DEFAULT_SERVICE_PATH)
 }
 
 // get stored service name
 func (p *service_pool) load_names() {
-	p.service_names = make(map[string]bool)
-	client := p.client_pool.Get().(*etcd.Client)
-	defer func() {
-		p.client_pool.Put(client)
-	}()
-
+	kAPI := etcdclient.NewKeysAPI(p.client)
 	// get the keys under directory
 	log.Info("reading names:", DEFAULT_NAME_FILE)
-	resp, err := client.Get(DEFAULT_NAME_FILE, false, false)
+	resp, err := kAPI.Get(context.Background(), DEFAULT_NAME_FILE, nil)
 	if err != nil {
 		log.Error(err)
 		return
@@ -93,7 +101,7 @@ func (p *service_pool) load_names() {
 	names := strings.Split(resp.Node.Value, "\n")
 	log.Info("all service names:", names)
 	for _, v := range names {
-		p.service_names[DEFAULT_SERVICE_PATH+"/"+strings.TrimSpace(v)] = true
+		p.known_names[DEFAULT_SERVICE_PATH+"/"+strings.TrimSpace(v)] = true
 	}
 
 	p.enable_name_check = true
@@ -101,14 +109,10 @@ func (p *service_pool) load_names() {
 
 // connect to all services
 func (p *service_pool) connect_all(directory string) {
-	client := p.client_pool.Get().(*etcd.Client)
-	defer func() {
-		p.client_pool.Put(client)
-	}()
-
+	kAPI := etcdclient.NewKeysAPI(p.client)
 	// get the keys under directory
 	log.Info("connecting services under:", directory)
-	resp, err := client.Get(directory, true, true)
+	resp, err := kAPI.Get(context.Background(), directory, &etcdclient.GetOptions{Recursive: true})
 	if err != nil {
 		log.Error(err)
 		return
@@ -134,39 +138,25 @@ func (p *service_pool) connect_all(directory string) {
 
 // watcher for data change in etcd directory
 func (p *service_pool) watcher() {
-	client := p.client_pool.Get().(*etcd.Client)
-	defer func() {
-		p.client_pool.Put(client)
-	}()
-
+	kAPI := etcdclient.NewKeysAPI(p.client)
+	w := kAPI.Watcher(DEFAULT_SERVICE_PATH, &etcdclient.WatcherOptions{Recursive: true})
 	for {
-		ch := make(chan *etcd.Response, 10)
-		go func() {
-			for {
-				if resp, ok := <-ch; ok {
-					if resp.Node.Dir {
-						continue
-					}
-					key, value := resp.Node.Key, resp.Node.Value
-					if value == "" {
-						log.Tracef("node delete: %v", key)
-						p.remove_service(key)
-					} else {
-						log.Tracef("node add: %v %v", key, value)
-						p.add_service(key, value)
-					}
-				} else {
-					return
-				}
-			}
-		}()
-
-		log.Info("Watching:", DEFAULT_SERVICE_PATH)
-		_, err := client.Watch(DEFAULT_SERVICE_PATH, 0, true, ch, nil)
+		resp, err := w.Next(context.Background())
 		if err != nil {
-			log.Critical(err)
+			log.Error(err)
+			continue
 		}
-		<-time.After(RETRY_DELAY)
+		if resp.Node.Dir {
+			continue
+		}
+		key, value := resp.Node.Key, resp.Node.Value
+		if value == "" {
+			log.Tracef("node delete: %v", key)
+			p.remove_service(key)
+		} else {
+			log.Tracef("node add: %v %v", key, value)
+			p.add_service(key, value)
+		}
 	}
 }
 
@@ -176,17 +166,19 @@ func (p *service_pool) add_service(key, value string) {
 	defer p.Unlock()
 	service_name := filepath.Dir(key)
 	// name check
-	if p.enable_name_check && !p.service_names[service_name] {
+	if p.enable_name_check && !p.known_names[service_name] {
 		log.Warningf("service not in names: %v, ignored", service_name)
 		return
 	}
 
+	// try new service kind init
 	if p.services[service_name] == nil {
 		p.services[service_name] = &service{}
 		log.Tracef("new service type: %v", service_name)
 	}
-	service := p.services[service_name]
 
+	// create service connection
+	service := p.services[service_name]
 	if conn, err := grpc.Dial(value, grpc.WithTimeout(DEFAULT_DIAL_TIMEOUT), grpc.WithInsecure()); err == nil {
 		service.clients = append(service.clients, client{key, conn})
 		log.Tracef("service added: %v -- %v", key, value)
@@ -199,6 +191,8 @@ func (p *service_pool) add_service(key, value string) {
 func (p *service_pool) remove_service(key string) {
 	p.Lock()
 	defer p.Unlock()
+
+	// check service kind
 	service_name := filepath.Dir(key)
 	service := p.services[service_name]
 	if service == nil {
@@ -206,6 +200,7 @@ func (p *service_pool) remove_service(key string) {
 		return
 	}
 
+	// remove a service
 	for k := range service.clients {
 		if service.clients[k].key == key { // deletion
 			service.clients = append(service.clients[:k], service.clients[k+1:]...)
@@ -218,19 +213,21 @@ func (p *service_pool) remove_service(key string) {
 // provide a specific key for a service, eg:
 // path:/backends/snowflake, id:s1
 //
-// service must be stored like /backends/xxx_service/xxx_id
+// the full cannonical path for this service is:
+// 			/backends/snowflake/s1
 func (p *service_pool) get_service_with_id(path string, id string) *grpc.ClientConn {
 	p.RLock()
 	defer p.RUnlock()
+	// check existence
 	service := p.services[path]
 	if service == nil {
 		return nil
 	}
-
 	if len(service.clients) == 0 {
 		return nil
 	}
 
+	// loop find a service with id
 	fullpath := string(path) + "/" + id
 	for k := range service.clients {
 		if service.clients[k].key == fullpath {
@@ -241,9 +238,12 @@ func (p *service_pool) get_service_with_id(path string, id string) *grpc.ClientC
 	return nil
 }
 
+// get a service in round-robin style
+// especially useful for load-balance with state-less services
 func (p *service_pool) get_service(path string) *grpc.ClientConn {
 	p.RLock()
 	defer p.RUnlock()
+	// check existence
 	service := p.services[path]
 	if service == nil {
 		return nil
@@ -252,16 +252,18 @@ func (p *service_pool) get_service(path string) *grpc.ClientConn {
 	if len(service.clients) == 0 {
 		return nil
 	}
+
+	// get a service in round-robind style,
 	idx := int(atomic.AddUint32(&service.idx, 1))
 	return service.clients[idx%len(service.clients)].conn
 }
 
-// choose a service randomly
+/////////////////////////////////////////////////////////////////
+// Wrappers
 func GetService(path string) *grpc.ClientConn {
 	return _default_pool.get_service(path)
 }
 
-// get a specific service instance with given path and id
 func GetServiceWithId(path string, id string) *grpc.ClientConn {
 	return _default_pool.get_service_with_id(path, id)
 }
--- services/services_test.go
+++ services/services_test.go
@@ -1,18 +1,20 @@
 package services
 
 import (
+	"github.com/davecgh/go-spew/spew"
 	"testing"
 )
 
 func TestService(t *testing.T) {
 	Init()
+	spew.Dump(_default_pool)
 	if GetService("/backends/snowflake") == nil {
 		t.Log("get service failed")
 	} else {
 		t.Log("get service succeed")
 	}
 
-	if GetServiceWithId("/backends/snowflake", "5946f18904ab:elated_blackwell:50003") == nil {
+	if GetServiceWithId("/backends/snowflake", "snowflake1") == nil {
 		t.Log("get service with id failed")
 	} else {
 		t.Log("get service with id succeed")
