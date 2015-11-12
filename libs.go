01bf199d1b31f314c7755b4f19e924b14d61654d
xtaci <daniel820313@gmail.com>
2015-11-12 12:41:35 +0800

    update deps

--- libs.go
+++ libs.go
@@ -1,1 +1,11 @@
 package libs
+
+import (
+	_ "github.com/coreos/etcd/client"
+	_ "github.com/gonet2/libs/nsq-logger"
+	_ "github.com/peterbourgon/g2s"
+	_ "github.com/pquerna/ffjson/ffjson"
+	_ "golang.org/x/net/context"
+	_ "google.golang.org/grpc"
+	_ "gopkg.in/mgo.v2/bson"
+)
